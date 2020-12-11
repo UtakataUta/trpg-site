9e31738558b197251b5a18922c74133a0ffd1729
yoshikawa0711 <b1017166@fun.ac.jp>
2020-12-12 07:16:35 +0900

    mod: ヘッダー画像の表示位置を調整

--- src/components/blogList.js
+++ src/components/blogList.js
@@ -43,9 +43,9 @@ const BlogList = () => {
           <article className="main-article">
             <div className="wrap-content">
               <header className="header-article">
-              <div className="icon"><Link to={node.fields.slug}><Image fluid={node.frontmatter.img.childImageSharp.fluid}/></Link></div>
                 <h2 className="title-article"><Link to={node.fields.slug}>{node.frontmatter.title}</Link></h2>
                 <div className="post-date"><span>{node.frontmatter.date}&nbsp;&nbsp;&nbsp;&nbsp;</span></div>
+                <div className="post-image"><Link to={node.fields.slug}><Image fluid={node.frontmatter.img.childImageSharp.fluid}/></Link></div>
               </header>
               <p>{node.frontmatter.description} <Link to={node.fields.slug}>(Read more ...)</Link></p>
             </div>
